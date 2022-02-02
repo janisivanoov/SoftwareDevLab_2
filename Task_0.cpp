2f43dbdbe636ac6035fd1d071f17a349fd242433
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-02-02 10:27:10 +0200

    Add files via upload
    updated version. fixed

--- Task_0.cpp
+++ Task_0.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int PI = 3.14;
+const int PI = 3.14; //setting the PI as a constant
 
 double sumToNumbers(double num1, double num2);
 double areaOfCircle(double radius);
@@ -14,13 +14,16 @@ void menu();
 int main(){
 
     int choiser;
-
-    menu();
+    do{
+    menu(); // applying the menu function
 
     cin>>choiser;
 
+    //Using switch to handle with unexpected input from the person via default part
     switch (choiser)
     {
+
+    //1st choise
     case 1:
         double num1, num2;
 
@@ -30,28 +33,33 @@ int main(){
         cout<<"Enter number 2: ";
         cin>>num2;
 
-        sumToNumbers(num1, num2);
+        sumToNumbers(num1, num2); //applying sum function
 
         break;
+
+    //2nd choise    
     case 2:
         double radius;
 
         cout<<"Enter the radius: ";
         cin>>radius;
 
-        areaOfCircle(radius);
+        areaOfCircle(radius); //applying area of circle founder function
 
         break;
+
+    //3rd choise
     case 3:
         double radius1;
 
         cout<<"Enter the radius: ";
         cin>>radius;
 
-        circumferenceOfCircle(radius1);
+        circumferenceOfCircle(radius1); //applying calculation the price with vat function
 
         break;
 
+    //4th choise
     case 4:
         double price;
         int vat;
@@ -62,43 +70,47 @@ int main(){
         cout<<"Enter the %: ";
         cin>>vat;
 
-        priceIncludingVAT(price, vat);
+        priceIncludingVAT(price, vat); //applying calculation the price with vat function
 
         break;
 
     case 5:
-
-        exit(1);
-
         break;
-
+    //Catching incorrect or unknown input
     default:
 
-        cout<<"Sorry, I don't know it! Try again!"<<endl;
-        
+        cout<<"Sorry, I don't know it!"<<endl;
+
         break;
-    }
+    } 
+
+    }while(choiser != 5);
 }
 
+//Sum function
 double sumToNumbers(double num1, double num2){
     cout<<"Sum is: "<<num1 + num2<<endl;
 }
 
+//Area of circle founder function
 double areaOfCircle(double radius){
     if(radius < 0){
-        cout<<"An error!";
+        cout<<"An error input!"<<endl;
     }
     cout<<"Area is "<<PI * radius * radius<<endl;
 }
 
+//Circuference of circle founder function
 double circumferenceOfCircle(double radius){
-    cout<<"Circumference of a circle is "<<PI * (radius + radius);
+    cout<<"Circumference of a circle is "<<PI * (radius + radius)<<endl;
 }
 
+//Calculation the price with vat function
 double priceIncludingVAT(double price, int vat){
-    cout<<"Price including the vat (given as %): "<< price + (price * vat / 100);
+    cout<<"Price including the vat (given as %): "<< price + (price * vat / 100)<<endl;
 }
 
+//Menu which we use in the beginning to make main function cleaner
 void menu(){
     cout<<"Select: "<<endl;
     cout<<"1) Sum two numbers"<<endl;
